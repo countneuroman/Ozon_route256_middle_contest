657f1c4a47728760b70cef486967ff6a25589a34
CountNeuroman <olorinoren@gmail.com>
2023-01-18 00:02:13 +0300

    Init solution

---
+++ AmountDue/AmountDue/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace AmountDue;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var dataCount = int.Parse(Console.ReadLine());
+        for (var i = 0; i < dataCount; i++)
+        {
+            var setCount = int.Parse(Console.ReadLine());
+            var allPricesInSet = Console.ReadLine().Split(' ').Select(x=>int.Parse(x)).ToArray();
+        }
+    }
+}
