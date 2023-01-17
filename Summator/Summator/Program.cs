bba243ddfcc9e2273a13d6851161066ac4ff38fd
CountNeuroman <olorinoren@gmail.com>
2023-01-17 23:50:15 +0300

    Add summator solutions

---
+++ Summator/Summator/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Summator;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var caseCount = int.Parse(Console.ReadLine());
+        
+        for (var i = 0; i < caseCount; i++)
+        {
+            int[] data = Console.ReadLine().Split(' ').Select(x=>int.Parse(x)).ToArray();
+            Console.WriteLine(data.Sum());
+        }
+    }
+}
