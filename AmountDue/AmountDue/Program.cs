11a60a556046bb74d31e020640a12b60f7db669e
CountNeuroman <olorinoren@gmail.com>
2023-01-18 21:23:09 +0300

    Add solution

--- AmountDue/AmountDue/Program.cs
+++ AmountDue/AmountDue/Program.cs
@@ -9,6 +9,27 @@ class Program
         {
             var setCount = int.Parse(Console.ReadLine());
             var allPricesInSet = Console.ReadLine().Split(' ').Select(x=>int.Parse(x)).ToArray();
+
+            Dictionary<int, int> prices = new Dictionary<int, int>();
+            foreach (var price in allPricesInSet)
+            {
+                if (prices.ContainsKey(price))
+                {
+                    prices[price]++;
+                    continue;
+                }
+                prices.Add(price, 1);
+            }
+
+            int result = 0;
+            foreach (var price in prices)
+            {
+                int actionValue = price.Value / 3;
+
+                result += (price.Value - actionValue) * price.Key;
+            }
+            
+            Console.WriteLine(result);
         }
     }
 }
