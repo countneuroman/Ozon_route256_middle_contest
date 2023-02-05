6cfcdcc9ad7e2e236a0cb608648948d5082ac4f6
CountNeuroman <olorinoren@gmail.com>
2023-02-05 10:48:48 +0300

    Add all tasks description

--- PairProgramming/PairProgramming/Program.cs
+++ PairProgramming/PairProgramming/Program.cs
@@ -1,3 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace PairProgramming;
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var caseCount = int.Parse(Console.ReadLine());
+        
+        for (var i = 0; i < caseCount; i++)
+        {
+            var dataCount = int.Parse(Console.ReadLine());
+            var data = Console.ReadLine().Split(' ').Select(x=>int.Parse(x)).ToList();
+
+            CheckPair(data);
+        }
+    }
+
+    private static void CheckPair(List<int> data)
+    {
+        var partnerProgrammerIndex = 0;
+        var partnerProgrammerMinRaznost = int.MaxValue;
+        for (var i = 0; i < data.Count; i++)
+        {
+            var currentProgrammer = data[i];
+            data.RemoveAt(i);
+            var datas = new List<int>(data);
+            for (var j = 0; j < datas.Count; j++)
+            {
+                if (partnerProgrammerMinRaznost > datas[j] / currentProgrammer)
+                {
+                    
+                }
+                
+            }
+        }
+        
+    }
+}
